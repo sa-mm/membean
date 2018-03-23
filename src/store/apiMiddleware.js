ab89c7f221f5aca0fa4fc95cb67f27a46e14d7bb
Sam <mcmyler@gmail.com>
2018-03-23 13:55:56 -0500

    Add middleware and root reducer

--- src/store/apiMiddleware.js
+++ src/store/apiMiddleware.js
@@ -1,8 +1,28 @@
-export default function () {
+export function apiFailure(type, error) {
+  return {
+    type: type + "_FAILURE",
+    payload: error.message,
+    error: true
+  };
+}
+
+export default function(store) {
   return next => action => {
-    switch (action.type) {
-      default:
-        return next(action);
+    if (!action.meta || action.meta.type !== "api") {
+      return next(action);
     }
+
+    action.payload
+      .then(apiResponse => {
+        const { meta, ...rest } = action;
+        store.dispatch({
+          ...rest,
+          payload: apiResponse
+        });
+      })
+      .catch(error => {
+        const failureAction = apiFailure(action.type, error);
+        store.dispatch(failureAction);
+      });
   };
 }
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,39 @@
+import mockFetch from "../api/mockFetch";
+
+//Constant
+const CALL_API = "CALL_API";
+const CALL_API_FAILURE = "CALL_API_FAILURE";
+
+//Action creator
+export function callApi() {
+  return {
+    type: CALL_API,
+    payload: mockFetch(),
+    meta: {
+      type: "api"
+    }
+  };
+}
+
+//Reducer
 const defaultState = {
-  apiResponse: null
+  apiResponse: null,
+  errorMessage: null
 };
 
 export default function(state = defaultState, action) {
-  switch(action.type) {
+  switch (action.type) {
+    case CALL_API:
+      return {
+        ...state,
+        apiResponse: action.payload,
+        errorMessage: null
+      };
+    case CALL_API_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload
+      };
     default:
       return state;
   }
